0dbc9142c1c734f577683c1888e42d4ff58d03e4
Haresh Chander <engrhareshchander@gmail.com>
2025-10-18 11:14:25 +0500

    Time: 0 ms (100%), Space: 17.7 MB (78.33%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+
+        first, second = 1, 2
+
+        for _ in range (3, n+1):
+            third = first + second  #Fibonacci SEries
+            first, second = second, third
+        return second
